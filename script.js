2d8bcafdbcc4bddc199492ba43838a9d7381cd42
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-08-25 22:14:35 +0500

    Set Map

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
 //   Rest , Spread 
-//  Array ko copy krna 
+ // 1.Array ko copy krna 
 
  let array1 = [1 , 2 , 3,4 , 5, , 7, 8 , 9, 10];
  let array2 = [...array1];
  
- console.log(`${array2} <br>`);
+ console.log(`${array2}`);
 
-//  Do array ko merge krna 
+ //  Output : 1,2,3,4,5,,7,8,9,10 
+
+// 2.  Do array ko merge krna 
 
  let fruit = ["mango" , "Apple"];
  let fruite = ["Banana" , "Cherry"];
@@ -15,7 +17,23 @@
 
  console.log(result);
 
- 
+//  Output :  ['mango', 'Apple', 'Banana', 'Cherry']
+
+//  3. Array me new value add krna 
+
+ let Fruite = ["Apple" , "Mango"];
+ let newFruite = [...Fruite , "Cherry"];
+
+ console.log(newFruite);
+
+//  Output :  ['Apple', 'Mango', 'Cherry']
+
+
+
+
+
+
+
 
  
 
