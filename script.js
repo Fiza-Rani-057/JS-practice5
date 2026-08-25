7da333f60e37d524e8e83ba92c629b3ba8922144
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-08-25 22:27:53 +0500

    Set Map

--- script.js
+++ script.js
@@ -64,3 +64,22 @@ console.log(newFruite);
 
   let newUser = {...user};
   console.log(newUser);
+
+   //  Output : {Name: 'Ali', age: 20}
+
+   // 8. Objects ko merge karna
+
+   let obj1 = {
+    name: "Ali"
+   }
+   let obj2 = {
+    age : 20
+   }
+ let person = {
+    ...obj1,
+    ...obj2
+ };
+
+  console.log(person);
+
+  // Output: {name: 'Ali', age: 20}
