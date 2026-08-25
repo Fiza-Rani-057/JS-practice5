87ab07615ca8634b8a7c1fd5d14c82ef05005408
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-08-25 22:24:21 +0500

    Set Map

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ let newFruite = [...Fruite, "Cherry"];
 
 console.log(newFruite);
 
-//  Output :  ['Apple', 'Mango', 'Cherry']
+ //  Output :  ['Apple', 'Mango', 'Cherry']
 
  // 4. Array ke start mein value add karna
 
@@ -37,5 +37,30 @@ console.log(newFruite);
 
  //  Output :  [1, 2, 3, 4, 5]
 
+ // 5. String ko array mein convert karna
 
+ let name = "Ali";
+ let letters = [...name];
 
+ console.log(letters);
+
+ //  Output : ['A', 'l', 'i']
+
+ // 6. Math.max() ke saath
+
+ let nums = [10 , 20 , 30 , 40 , 8];
+ let maxValue = [Math.max(...nums)];
+
+ console.log(maxValue);
+
+ //  Output :[40]
+
+ // 7. Object ko copy karna
+
+  let user = {
+    Name : "Ali",
+    age : 20
+  };
+
+  let newUser = {...user};
+  console.log(newUser);
