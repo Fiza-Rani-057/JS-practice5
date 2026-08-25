764c716a00e3600b015840cbf8eece37000f1750
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-08-25 22:17:07 +0500

    Set Map

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 
 //   Rest , Spread 
- // 1.Array ko copy krna 
+// 1.Array ko copy krna 
 
- let array1 = [1 , 2 , 3,4 , 5, , 7, 8 , 9, 10];
- let array2 = [...array1];
- 
- console.log(`${array2}`);
+let array1 = [1, 2, 3, 4, 5, , 7, 8, 9, 10];
+let array2 = [...array1];
 
- //  Output : 1,2,3,4,5,,7,8,9,10 
+console.log(`${array2}`);
+
+//  Output : 1,2,3,4,5,,7,8,9,10 
 
 // 2.  Do array ko merge krna 
 
- let fruit = ["mango" , "Apple"];
- let fruite = ["Banana" , "Cherry"];
- const result = [...fruit , ...fruite];
+let fruit = ["mango", "Apple"];
+let fruite = ["Banana", "Cherry"];
+const result = [...fruit, ...fruite];
 
- console.log(result);
+console.log(result);
 
 //  Output :  ['mango', 'Apple', 'Banana', 'Cherry']
 
 //  3. Array me new value add krna 
 
- let Fruite = ["Apple" , "Mango"];
- let newFruite = [...Fruite , "Cherry"];
+let Fruite = ["Apple", "Mango"];
+let newFruite = [...Fruite, "Cherry"];
 
- console.log(newFruite);
+console.log(newFruite);
 
 //  Output :  ['Apple', 'Mango', 'Cherry']
 
+ // 4. Array ke start mein value add karna
 
-
-
-
-
-
-
+ let numbers = [2 , 3 , 4, 5];
+ let newNumbers = [1, ...numbers];
  
+ console.log(newNumbers);
+
+ //  Output :  [1, 2, 3, 4, 5]
+
 
 
