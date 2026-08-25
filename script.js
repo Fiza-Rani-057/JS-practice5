bd8e59ab961ae3780eaed58e47ec001e2cc3313b
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-08-25 22:08:46 +0500

    Set Map

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//   Rest , Spread 
+//  Array ko copy krna 
+
+ let array1 = [1 , 2 , 3,4 , 5, , 7, 8 , 9, 10];
+ let array2 = [...array1];
+ 
+ console.log(`${array2} <br>`);
+
+//  Do array ko merge krna 
+
+ let fruit = ["mango" , "Apple"];
+ let fruite = ["Banana" , "Cherry"];
+ const result = [...fruit , ...fruite];
+
+ console.log(result);
+
+ 
+
+ 
+
+
